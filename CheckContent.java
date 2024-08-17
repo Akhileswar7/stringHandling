0d9fcf7da4ede563d2ee050290b7bf972c3b91a8
akhil <akhilgajjala7@gmail.com>
2024-08-17 17:46:19 +0530

    commit

--- CheckContent.java
+++ CheckContent.java
@@ -1,16 +1,16 @@
 package com.vedait.assignments.stringHandling;
 
 public class CheckContent {
-	
+
 	public static void main(String[] args) {
-		
-		String string="java";
-		String string2="java";
-		System.out.println(string==string2);
+
+		String string = "java";
+		String string2 = "java";
+		System.out.println(string == string2);
 		System.out.println(string.compareTo(string2));
+		System.out.println(string.equals(string2));
 		System.out.println(string.contentEquals(string2));
 		System.out.println(string.equals(string2));
-		System.out.println(string.contentEquals(string2)?"Content matched":"Content not matched");
-		
+		System.out.println(string.contentEquals(string2) ? "Content matched" : "Content not matched");
 	}
 }
